b8657c84a0d446bc2c3033ddcb0f18727b6b4777
Krier <vinoshan@outlook.com>
2023-09-06 22:52:58 -0400

    Added authController.js code

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,20 +1,95 @@
+// controllers/authController.js
 
 const User = require('../models/User');
+
 const authController = {
   register: async (req, res) => {
-    // Handle user registration logic
+    try {
+      // Extract user registration data from the request body
+      const { username, email, password } = req.body;
+
+      // Validate user input
+      // Check if the user already exists in the database
+      const existingUser = await User.findOne({ where: { email } });
+
+      if (existingUser) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+
+      // Create a new user in the database
+      const newUser = await User.create({ username, email, password });
+
+      // Set up a session for the new user
+      req.session.userId = newUser.id;
+
+      // Respond with a success message or redirect to a profile page
+      res.json({ success: true, message: 'User registered successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   },
 
   login: async (req, res) => {
-    // Handle user login logic
+    try {
+      // Extract user login data from the request body
+      const { email, password } = req.body;
+
+      // Verify user credentials
+      const user = await User.findOne({ where: { email } });
+
+      if (!user || !user.comparePassword(password)) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+
+      // Set up a session for the authenticated user
+      req.session.userId = user.id;
+
+      // Respond with a success message or redirect to a dashboard page
+      res.json({ success: true, message: 'Login successful' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   },
 
   logout: async (req, res) => {
-    // Handle user logout logic
+    try {
+      // Clear the user's session
+      req.session.destroy();
+
+      // Redirect to the login page or respond with a success message
+      res.json({ success: true, message: 'Logout successful' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   },
 
   resetPassword: async (req, res) => {
-    // Handle password reset logic
+    try {
+      // Extract user input
+      const { email } = req.body;
+
+      // Check if the user exists
+      const user = await User.findOne({ where: { email } });
+
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      // Generate a password reset token
+      const resetToken = generateResetToken();
+
+      // Send an email with the resetToken to the user's email address
+      sendResetEmail(email, resetToken);
+
+      // Respond with a success message or instruct the user to check their email
+      res.json({ success: true, message: 'Password reset email sent' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   },
 };
 
