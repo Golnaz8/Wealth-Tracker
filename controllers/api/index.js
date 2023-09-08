b95ac9968b9403b90c5ac8683adcda59696ad433
Ramen <134460692+ramendosanjh@users.noreply.github.com>
2023-09-07 20:01:05 -0400

    Revert "pull from main "

--- controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const authRoutes = require('./authRoutes');
-const transactionRoutes = require('./transactionRoutes');
-
-router.use('/users', authRoutes);
-router.use('/transactions', transactionRoutes);
-
-module.exports = router;
