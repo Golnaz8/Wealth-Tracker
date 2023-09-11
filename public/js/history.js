523d1352aad9454aeff24bceb041eadd4ff02a6d
Golnaz8 <golnaz.brj@gmail.com>
2023-09-11 15:55:40 -0400

    added feature update and delete for history page

---
+++ public/js/history.js
@@ -0,0 +1,30 @@
+// Function to handle the deletion of a transaction
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    try {
+      const response = await fetch(`/api/transactions/${id}`, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        throw new Error('Failed to delete transaction');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('An error occurred while deleting the transaction.');
+    }
+  }
+};
+
+
+
+const transactionButtons = document.querySelectorAll('.transaction-list button');
+transactionButtons.forEach((button) => {
+  button.addEventListener('click', delButtonHandler);
+});
+
+
